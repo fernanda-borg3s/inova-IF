7420abe9f43ca5188a42f69dd1db8b5d6218865e
Fernanda Borges <fernandaborgesb4@gmail.com>
2024-03-08 20:39:37 -0300

    upload dos arquivos

---
+++ 1_tent_front/src/Data.js
@@ -0,0 +1,55 @@
+export const encontros = [
+    {
+        id: 1,
+        salas: "207-A",
+        dataInicio : "24/05/2024",
+        horaInicio: "8:00",
+        dataFim: "15/12/2024",
+        horaFim: "11:00",
+        dataCadastro: "20/05/2024",
+        matProfessora: 1234567,
+        tituloEncontro: "Lorem ipsum 1",
+        bolRepete: true,
+        descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididun"
+    },
+    {
+        id: 2,
+        salas: "109-D",
+        dataInicio : "24/02/2024",
+        horaInicio: "8:20",
+        dataFim: "15/12/2024",
+        horaFim: "12:00",
+        dataCadastro: "20/05/2024",
+        matProfessora: 6789001,
+        tituloEncontro: "Lorem ipsum Dolor",
+        bolRepete: false,
+        descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididun"
+    },
+    {
+        id: 3,
+        salas: "207-B",
+        dataInicio : "08/05/2024",
+        horaInicio: "8:10",
+        dataFim: "15/12/2024",
+        horaFim: "11:40",
+        dataCadastro: "20/05/2024",
+        matProfessora: 2222338,
+        tituloEncontro: "Lorem ipsum 2",
+        bolRepete: true,
+        descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididun"
+    },
+    {
+        id: 4,
+        salas: "208-A",
+        dataInicio : "24/03/2024",
+        horaInicio: "8:00",
+        dataFim: "05/11/2024",
+        horaFim: "12:10",
+        dataCadastro: "12/03/2024",
+        matProfessora: 1234567,
+        tituloEncontro: "Lorem ipsum 3",
+        bolRepete: false,
+        descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididun"
+    }
+
+]
---
+++ 1_tent_front/src/pages/Home/Home.jsx
@@ -0,0 +1,11 @@
+import { NavbarC } from "../../components/Navbar/Navbar";
+
+export default function Home(){
+    return (
+    <>
+        <NavbarC/>
+        <h1>HEYYY</h1>
+    </>
+    
+    )
+}
